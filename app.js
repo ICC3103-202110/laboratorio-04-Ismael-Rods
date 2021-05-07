c162ac1ca9ccda2acbe69cf6d4ef217c1aa76a88
Ismael-Rods <irodriguez5@miuandes.cl>
2021-05-07 00:34:45 -0400

    view counter function update and app function

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 //first commit
 function view(counter) {
-    return `Counter: ${counter}`
+    return `
+            Counter: ${counter}\n
+            (+) (-)\n
+            (q) for quit\n`;
     
 }
 const update = (msg,counter) => {
     if (msg === '+')
-        return counter +1
+        return counter +1;
     else if (msg === '-')
-        return counter -1
+        return counter -1;
 }   
-console.log(update('-',5))
+"use strict";
+const prompt = require('prompt-sync')();
+
+function app(counter) {
+    
+    while(true){
+        try {
+
+            console.log(view(counter))
+            let  input = prompt('What would you do?');
+            console.clear();
+            if (input==='q')
+                break;
+            else if (input==='+' || input==='-') {
+                counter = update(input,counter);
+            }
+            else {
+                throw 'Error enter (+) (-) (q)'
+            }
+        }
+      catch(e) {
+        console.log(e);
+      } 
+    }
+}
+
+
+console.log(app(0));
